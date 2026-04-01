321a88f6e3396d864a73563e90d69e54c1a13d60
Luca-Python <luca-python@no-mail.de>
2026-04-01 17:01:06 +0200

    Started with landing page -> programming animation

--- public/dashboard/home.js
+++ public/dashboard/home.js
@@ -1,1 +1,34 @@
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.38.0/min/vs' }});
+require(['vs/editor/editor.main'], function() {
+    var editor = monaco.editor.create(document.getElementById('editor'), {
+        value: "",
+        language: "python",
+        theme: "vs-dark",
+        fontSize: 16,
+        automaticLayout: true
+    });
 
+    const code = `class NexoryOrg:
+    def __init__(self):
+    self.name = name
+
+    def join(self, person):
+        print(f"{person} has joined {self.name}!")
+
+if __name__ == "__main__":
+    orga = NexoryOrg("Nexory.Org")
+    orga.join("YOU")
+    orga.join("NOW")
+
+    print("Welcome to Nexory.Org")`;
+
+    let i = 0;
+    function typeCode() {
+        if (i <= code.length) {
+            editor.setValue(code.slice(0, i));
+            i++;
+            setTimeout(typeCode, 5);
+        }
+    }
+    typeCode();
+});
--- public/dashboard/home.php
+++ public/dashboard/home.php
@@ -12,14 +12,25 @@ require_once __DIR__ . '/../init.php';
         <base href="/">
         <title><?=  e(t('home.title')) ?> </title>
         <link rel="stylesheet" href="../dashboard/home.css">
+        <link rel="stylesheet" href="../navebar/navebar.css">
+        <link rel="stylesheet" href="../footer/footer.css">
     </head>
 
     <body>
         <?php include __DIR__ . '/../navebar/navebar.php'; ?>
 
-        <section class="test">
-            <h1>test</h1>
+        <section class="hero">
+            <div class="infor">
+                <h1>Nexory.Org</h1>
+                <p>Open source projects - 5+ years of experience - Python, JavaScript, PHP</p>
+            </div>
+            <div id="editor" style="height: 400px; width: 600px;"></div>
         </section>
+
+        <?php include __DIR__ . '/../footer/footer.php'; ?>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.38.0/min/vs/loader.min.js"></script>
+        <script src="../dashboard/home.js"></script>
     </body>
 
 </html>
